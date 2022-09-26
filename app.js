7818ffe90b468f144a12865aab972e3452abe12d
longcircle <cwg03125@gmail.com>
2022-09-26 18:46:22 +0900

    2022.09.26

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const title = document.querySelector(".hello h1");
 
-title.addEventListener;
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
