72aab91ad3e17e7f0ab756fc028b780a39dba23a
longcircle <cwg03125@gmail.com>
2022-09-30 17:48:18 +0900

    2022.09.30

--- app.js
+++ app.js
@@ -10,18 +10,23 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  grteeing.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  //   grteeing.innerText = `Hello ${username}`;
+  //   greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function paintGreetings() {
+  grteeing.innerText = `Hello ${Uname}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  grteeing.innerText = `Hello ${savedUsername}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  // grteeing.innerText = `Hello ${savedUsername}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings();
 }
